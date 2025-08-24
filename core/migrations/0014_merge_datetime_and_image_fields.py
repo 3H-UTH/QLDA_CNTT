3bdb6089d092eb8bf56d11ada3f4a59b1684eb74
Vu Truong Hung <vhung9955@gmail.com>
2025-08-25 01:15:46 +0700

    ok
    ok

---
+++ core/migrations/0014_merge_datetime_and_image_fields.py
@@ -0,0 +1,14 @@
+# Generated merge migration
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0001_initial'),
+    ]
+
+    operations = [
+        # No operations needed, just merging the branches
+    ]
